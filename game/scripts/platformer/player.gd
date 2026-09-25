8749f1c4829825d4729080416b903537d125eb82
spdskatr <25078279+spdskatr@users.noreply.github.com>
2026-09-26 02:50:26 +0800

    stop tweaking platforming mechanics

--- game/scripts/platformer/player.gd
+++ game/scripts/platformer/player.gd
@@ -1,25 +1,67 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const MAX_SPEED = 600.0
+const JUMP_SPEED = -450.0
 
+const NOT_ON_FLOOR_PENALTY : float = 0.25
+const BASE_ACCEL : float = 400
+const START_ACCEL_FACTOR : float = 4
+const DECEL : float = BASE_ACCEL * START_ACCEL_FACTOR
+const AIR_RESISTANCE : float = 0.2
+const MAX_AIR_DECEL : float = MAX_SPEED * 0.15
+const JUMP_PERIOD : float = 0.08
+const COYOTE_TIME : float = 0.7  # approx 2 physics frames
+
+# Speed management
+var speed_x : float = 0.0
+var speed_y : float = 0.0
+
+# Acceleration is a state machine
+# Time since last jump
+var jump_time : float = 0.0
+# Time since touching the ground
+var air_time : float = 0.0
+
+static func get_x_accel(signed_speed : float, on_floor : bool) -> float:
+	var not_on_floor_penalty : float = 1 if on_floor or signed_speed < 0 else NOT_ON_FLOOR_PENALTY
+	var accel_factor : float = (START_ACCEL_FACTOR + (1 - START_ACCEL_FACTOR) * signed_speed / (MAX_SPEED))
+	return BASE_ACCEL * not_on_floor_penalty * accel_factor
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	var jumping : bool = false
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		air_time += delta
+	else:
+		air_time = 0.0
 
 	# Handle jump.
-	if Input.is_action_just_pressed("PlatformerJump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("PlatformerJump") and air_time < COYOTE_TIME:
+		air_time = COYOTE_TIME
+		jump_time = 0.0
+		jumping = true
+	elif Input.is_action_pressed("PlatformerJump") and jump_time < JUMP_PERIOD:
+		jumping = true
+	
+	if jumping:
+		var jump_delta = min(delta, JUMP_PERIOD - jump_time)
+		velocity.y += JUMP_SPEED * jump_delta / JUMP_PERIOD
+	else:
+		# Fall due to gravity
+		var grav_factor : float = 0.8 if Input.is_action_pressed("PlatformerJump") else 1.0
+		velocity += get_gravity() * delta * grav_factor
+	
+	
+	jump_time += delta
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if direction != 0:
+		velocity.x = move_toward(velocity.x, direction * MAX_SPEED, delta * get_x_accel(velocity.x * direction, is_on_floor()))
+		if not is_on_floor():
+			velocity.x -= sign(velocity.x) * delta * min(MAX_AIR_DECEL, abs(velocity.x) * AIR_RESISTANCE)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, delta * DECEL)
 
 	move_and_slide()
