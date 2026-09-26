1605a9c4d05543a775469d1b9051723e611ad858
spdskatr <25078279+spdskatr@users.noreply.github.com>
2026-09-26 16:57:53 +0800

    Finish interactables

--- game/scripts/platformer/interactable_sprite.gd
+++ game/scripts/platformer/interactable_sprite.gd
@@ -2,6 +2,7 @@ extends Sprite2D
 signal interacted
 
 @export var interactableId: String = "Unknown"
+@export var interactedSprite: Texture2D = null
 
 const INACTIVE_OUTLINE: float = 0.0
 const IN_RANGE_OUTLINE: float = 2.0 # outline width in game pixels
@@ -23,10 +24,16 @@ func show_state() -> void:
 		"INACTIVE":
 			set_instance_shader_parameter("outLineSize", INACTIVE_OUTLINE)
 			set_instance_shader_parameter("outLineColor", IN_RANGE_COLOR)
+		"DISABLED":
+			set_instance_shader_parameter("outLineSize", INACTIVE_OUTLINE)
+			set_instance_shader_parameter("outLineColor", IN_RANGE_COLOR)
 		_:
 			push_warning("Unknown state ", interactionState)
 
 func interact() -> void:
+	texture = interactedSprite
+	interactionState = "DISABLED"
+	show_state()
 	interacted.emit()
 
 # The shader can only draw inside the sprite's rect, so grow the drawn region past the
@@ -44,13 +51,15 @@ func _ready() -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	interactionState = "IN_RANGE"
-	show_state()
+	if interactionState != "DISABLED":
+		interactionState = "IN_RANGE"
+		show_state()
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	interactionState = "INACTIVE"
-	show_state()
+	if interactionState != "DISABLED":
+		interactionState = "INACTIVE"
+		show_state()
 
 func _physics_process(delta: float) -> void:
 	if interactionState == "IN_RANGE" and Input.is_action_just_pressed("Interact"):
